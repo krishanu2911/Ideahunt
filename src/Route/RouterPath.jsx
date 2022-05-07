fbe0e15c9d0fc126a0286e4ac1d9f997aba3b5cc
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-07 11:44:02 +0530

    Feat-Router Setup

---
+++ src/Route/RouterPath.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup, Home, Explore, Profile } from "../Pages/index";
+function RouterPath() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/Profile" element={<Profile />} />
+    </Routes>
+  );
+}
+export default RouterPath;
