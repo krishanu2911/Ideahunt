2841c52ea80f4b178a3ca8013025cc272b10e355
nishasen <43262505+nishasen@users.noreply.github.com>
2022-05-07 14:26:07 +0530

    Feat: topnav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import RouterPath from 'Route/RouterPath';
+import { Topnav } from 'Components';
+import { useTheme } from 'Context';
 function App() {
+  const { themeState } = useTheme();
+  const { theme } = themeState;
   return (
-    <div className="App">
+    <div className={`App ${theme==="light" ? "app-light" : "app-dark"}`}>
       <main className='main-section'>
             {/* here add the component with the same class name */}
-            <nav className='nav-section'>top nav bar</nav>
+            <nav className='nav-section'>
+              <Topnav />
+            </nav>
             <RouterPath />
             <footer className='footer-section'>
                 footer
