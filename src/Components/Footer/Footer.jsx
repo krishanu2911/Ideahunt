5804338d9f39c0f9f1f12963b5ffb9545c30cd71
nishasen <43262505+nishasen@users.noreply.github.com>
2022-05-08 23:24:22 +0530

    fixed footer colors

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
+import react from 'react';
+import { useTheme } from 'Context';
 import style from './Footer.module.css';
 
 const react_icon = 'https://ik.imagekit.io/ecomdiagonalley/Ideahunt/structure-removebg-preview_wgChIrEpR.png?ik-sdk-version=javascript-1.4.3&updatedAt=1651942971483';
 const Footer = () => {
+  const { themeState } = useTheme();
+  const { theme } = themeState;
+  const footerColor = theme==="light" ? style.footer_light : style.footer_dark
   return (
-    <div className={style.footer}>
+    <div className={`${style.footer} ${footerColor}`}>
         Made with <img src={react_icon} alt="react icon" className={style.icons}/> by <span className="span-text">Hoisters</span>
     </div>
   )
